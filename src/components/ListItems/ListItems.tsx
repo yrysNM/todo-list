e3ce01b45a5c0d37bf287c30fd8f4f34ef4f076d
yrysNM <yrys.n3m@gmail.com>
2023-04-12 23:34:32 +0600

    drag drop items

--- src/components/ListItems/ListItems.tsx
+++ src/components/ListItems/ListItems.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from "react";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
 
-import { setSearchItems } from "../../redux/tool/ItemsSlice";
+import { setItems, setSearchItems } from "../../redux/tool/ItemsSlice";
 import { useAppSelector, useAppDispatch } from "../../hooks/redux.hook";
 import { ListItemsLayout } from "../layouts/ListItemsLayout";
 import { ItemBtns } from "../ItemBtns";
@@ -13,6 +19,16 @@ export const ListItems = () => {
   const { items, searchItems } = useAppSelector((state) => state.items);
   const { methodSort } = useAppSelector((state) => state.view);
   const dispatch = useAppDispatch();
+
+  const handleOnDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+
+    const itemsList = Array.from(items);
+    const [reorderedItem] = itemsList.splice(result.source.index, 1);
+    itemsList.splice(result.destination.index, 0, reorderedItem);
+
+    dispatch(setItems(itemsList));
+  };
 
   useEffect(() => {
     if (methodSort.method === "name") {
@@ -50,19 +66,36 @@ export const ListItems = () => {
 
   const ShowItems = (itemList: IArchiveItem[]) => {
     return (
-      <>
-        {itemList.map((item) => (
-          <ListItemsLayout key={item.id}>
-            <ItemInfo
-              content={item.content}
-              description={item.description}
-              is_completed={item.is_completed}
-              id={item.id}
-            />
-            <ItemBtns task_id={item.id} content={item.content} />
-          </ListItemsLayout>
-        ))}
-      </>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="items">
+          {(provided) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {itemList.map((item, i) => (
+                <Draggable key={item.id} draggableId={item.id} index={i}>
+                  {(provided) => (
+                    <li
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      <ListItemsLayout>
+                        <ItemInfo
+                          content={item.content}
+                          description={item.description}
+                          is_completed={item.is_completed}
+                          id={item.id}
+                        />
+                        <ItemBtns task_id={item.id} content={item.content} />
+                      </ListItemsLayout>
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     );
   };
 
