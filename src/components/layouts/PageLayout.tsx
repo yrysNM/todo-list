cef6085046ce17195d6e93b033214b139822b96a
yrysNM <yrys.n3m@gmail.com>
2023-04-19 12:38:38 +0600

    style

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Lottie from "lottie-react";
 
+import { useAppSelector } from "../../hooks/redux.hook";
 import backgroundAnimation from "../../assets/json/backgroundAnimation.json";
 
 import { IGeneralChildren } from "../../Interfaces/IGeneralComponent";
 import { Header } from "../Header";
 
 export const PageLayout = ({ children }: IGeneralChildren) => {
+  const { items } = useAppSelector((state) => state.items);
+
   return (
     <React.Fragment>
       <Header />
@@ -14,6 +17,7 @@ export const PageLayout = ({ children }: IGeneralChildren) => {
       <Lottie
         animationData={backgroundAnimation}
         className="templateAnimation"
+        style={{ top: items.length < 4 && items.length !== 0 ? "100%" : "50%" }}
       />
     </React.Fragment>
   );
